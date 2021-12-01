f46ffe4d9de32426ca3650f1ecb96199ed91a3c9
Maxime Dolores <19026937+mdolr@users.noreply.github.com>
2021-12-01 16:49:22 +0100

    docs: comments

--- src/implementations/FordFulkerson.java
+++ src/implementations/FordFulkerson.java
@@ -7,7 +7,13 @@ public class FordFulkerson {
   // Return le reseau avec son flot maximal
   // Algorithme de Ford Fulkerson
   public Reseau calculFlotMax(Reseau reseau) {
+    // Commencer à partir de la source:
+    // Tant qu'il existe un arc sortant avec flot < capacité:
+    // Passer la sauce (ajouter val flot)
 
+    // Ou plutôt :
+    // Si existance un chemin de croissance (avoir réseau résiduel?)
+    // Ajouter val flot le plus petit sur chaque arc (faire -val si arc retour)
 
     return null;
   }
